beeba936ec152d50d136890373a4a188bd8ff220
Siddharth Shah <siddharth.shah@mail.com>
2025-02-23 12:01:18 +0530

    first draft

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import sharp from 'sharp';
+
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react(), icon()],
+  assets: {
+    addSharpInstructions: true,
+    services: [
+      {
+        name: 'sharp',
+        constructor: sharp,
+      },
+    ],
+  },
+});
